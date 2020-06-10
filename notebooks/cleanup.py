37ac92b09006bce7414084ae68cd6adf6e873315
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-06-10 11:40:55 +0200

    [feature]: add position to noise dataset
    An updated events dataset was provided by Roel on 2020-06-09 which was
    used instead of the original. Position and direction data was added to
    the noise dataset and saved to disk. notebooks/cleanup.py was updated to
    reflect all efforts which were made to obtain the final datasets
    (noise.csv and hits.csv).

--- notebooks/cleanup.py
+++ notebooks/cleanup.py
@@ -1,42 +1,66 @@
 import pandas as pd
 
 """
-# load the dataset
+# Noise dataset
+read the original noise dataset (without x,y,z,dx,dy & dz)
 """
-events = pd.read_hdf("data/events.h5")
+
 noise = pd.read_csv("data/noise.csv")
 
 """
-# expore `noise`
-"""
-noise.shape
-noise.head
-
-"""
-make sure there are no missing values (there aren't any)
-"""
-noise.isna().any().any()
-
-"""
-# check if noise.time is unique (**it isn't**)
-NOTE this method takes a good few seconds to run
-NOTE so we cannot use it as index column
-NOTE it makes sense why it's not unique, we may have hits in differnt DOMs at
-the same time
-"""
-# noise.time.is_unique
-
-"""
-# explore `events`
-`events` contains 18 columns however the each item is inside a (rather
-redundant) list, so we first flatten it.
+add a column containing the corresponding id in the positions array. The
+formula was provided to us by Roel.
 """
 
-events = events.applymap(lambda x: x[0])
-events.head()
-events.shape
+noise["pos_idx"] = 31 * (noise["dom id"] - 1) + noise["pmt id"]
 
 """
-save this for future work
+Next convert numpy array to pandas dataframe.
+NOTE run notebooks/parse_detx.py and have `positions` in memory
 """
-events.to_csv("data/events.csv", index=False)
+
+pos = pd.DataFrame(positions)
+pos['pos_idx'] = pos.index
+
+"""
+Finally, combine the two, cleanup and save to disk.
+"""
+
+df = pd.merge(noise, pos, on='pos_idx')
+df = df.drop(columns=['pos_idx'])
+df = df.rename(columns={'dom id': 'dom_id', 'pmt id': 'pmt_id',
+    'time-over-threshold': 'tot', 'x': 'pos_x', 'y' : 'pos_y', 'z': 'pos_z',
+    'dx': 'dir_x', 'dy': 'dir_y', 'dz': 'dir_z'})
+
+"""
+# Events dataset
+NOTE One Jun 09, 2020 Roel provided a new, cleanup up events dataset.
+We do not care for `mc_info` for the time being
+"""
+
+mc_info = pd.read_hdf("data/events.h5", key="/data/mc_info")
+mc_hits = pd.read_hdf("data/events.h5", key="/data/mc_hits")
+
+"""
+align the column names of `mc_hits` with that of noise
+"""
+
+mc_hits = mc_hits.rename(columns={'h.dom_id': 'dom_id', 'h.pmt_id': 'pmt_id',
+    'h.pos.x': 'pos_x', 'h.pos.y': 'pos_y', 'h.pos.z': 'pos_z', 'h.dir.x':
+    'dir_x', 'h.dir.y': 'dir_y', 'h.dir.z': 'dir_z', 'h.tot': 'tot', 'h.t':
+    'time'})
+
+"""
+add labels
+"""
+
+df["label"] = 0
+mc_hits["label"] = 1
+
+
+"""
+and save to disk.
+"""
+
+mc_hits.to_csv("data/hits.csv", index=False)
+df.to_csv("data/noise.csv", index=False)
