3721ff4d97b017ad245e08f52b4e6854028f40c1
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-08-05 12:30:19 +0200

    [refactor]: generalize data generation script
    The script can now be used to generate data suitable for the pattern
    matrix MLP.

--- scripts/generate-pattern-matrix-data.py
+++ scripts/generate-pattern-matrix-data.py
@@ -1,12 +1,24 @@
 from context import km3net
 from km3net.utils import DATADIR
 import km3net.data.pattern_matrix as pm
+import pandas as pd
 import sys
+import os
 
 if __name__ == "__main__":
-    path = DATADIR + '/processed/slice-615.csv'
     forward = 'n'
-    frac = float(sys.argv[1])
+    path = DATADIR + sys.argv[1]
+    frac = float(sys.argv[2])
+
+    while not os.path.isfile(path):
+        print('{0} does not exist!'.format(path))
+        path = DATADIR+input('path? :')
+
+    print("data file: {0}, frac: {1}".format(path, frac))
+    master = input("proceed? [y/n]: ")
+
+    if master.lower() == 'n': exit()
+
     df = pd.read_csv(path)
 
     # make sure the distribution is acceptable before proceeding
@@ -27,17 +39,19 @@ if __name__ == "__main__":
     print("Shape of unrelated: {0}".format(sample[sample['label'] == 0].shape))
 
     # equalize tragets
-    print("equalizing targets...")
-    sample = pm.equalise_targets(sample)
-    print("---")
-    print("Shape of equalised sample: {0}".format(sample.shape))
-    print("Shape of related: {0}".format(sample[sample['label'] == 1].shape))
-    print("Shape of unrelated: {0}".format(sample[sample['label'] == 0].shape))
+    equalise = input("equalise targets? [y/n]: ")
+    if equalise.lower() == 'y':
+        print("equalizing targets...")
+        sample = pm.equalise_targets(sample)
+        print("---")
+        print("Shape of equalised sample: {0}".format(sample.shape))
+        print("Shape of related: {0}".format(sample[sample['label'] == 1].shape))
+        print("Shape of unrelated: {0}".format(sample[sample['label'] == 0].shape))
 
     save = input("save to disk? [y/n]: ")
     if save.lower() == 'y':
-        sample.to_csv(DATADIR+'/train/slice-615-25-equal.csv', index=False,
-                header=False)
+        name = input("name? :")
+        sample.to_csv(DATADIR+name, index=False, header=False)
 
     print("DONE")
 
