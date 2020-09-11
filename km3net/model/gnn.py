89a268a3627667d57ad9a706b2688d2b9d50c39d
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-09-11 11:21:56 +0200

    [refactor]: update km3net.model.gnn
    This commit implements the GNN model using pytorch_geometric (rather
    than  DGL).

--- km3net/model/gnn.py
+++ km3net/model/gnn.py
@@ -1,31 +1,23 @@
-import dgl
-from torch.nn import Linear, ReLU, Sigmoid, Module
-from torch.nn.init import kaiming_uniform_, xavier_uniform_
-from dgl.nn.pytorch import GraphConv
+import torch.nn.functional as F
+from torch.nn import Module
+from torch_geometric.nn import GCNConv
 
 class GNN(Module):
-    def __init__(self, in_dim, hidden_dim):
+    def __init__(self, in_dim):
         super(GNN, self).__init__()
-        self.conv1 = GraphConv(in_dim, hidden_dim)
-        kaiming_uniform_(self.conv1.weight, nonlinearity='relu')
-        self.act1 = ReLU()
-        self.conv2 = GraphConv(hidden_dim, hidden_dim)
-        kaiming_uniform_(self.conv2.weight, nonlinearity='relu')
-        self.act2 = ReLU()
-        self.hidden1 = Linear(hidden_dim, 1)
-        xavier_uniform_(self.hidden1.weight)
-        self.act3 = Sigmoid()
+        self.conv1 = GCNConv(in_dim, 16)
+        self.conv2 = GCNConv(16, 2)
+        self.linear1 = Linear(2, 1)
 
-    def forward(self, g):
-        h = g.in_degrees().view(-1, 1)
-        h = self.conv1(g, h)
-        h = self.act1(h)
-        h = self.conv2(g, h)
-        h = self.act2(h)
-        g.ndata['h'] = h
-        hg = dgl.mean_nodes(g, 'h')
-        hg = self.hidden1(hg)
-        hg = self.act3(hg)
+    def forward(self, G):
+        x, edge_index, edge_weight = G.x, G.edge_index, G.edge_attr
+        x = self.conv1(x, edge_index, edge_weight)
+        x = x.relu()
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index, edge_weight)
+        out = x.relu()
+        out = self.linear1(out)
+        out = out.sigmoid()
 
-        return hg
+        return out, x
 
